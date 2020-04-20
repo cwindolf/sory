1f1d098f675829757863d3e427d3524548a39f7d
Charlie Windolf <cwindolf95@gmail.com>
2020-04-20 12:44:37 -0400

    Add types to the model.

--- sory/model.py
+++ sory/model.py
@@ -3,14 +3,21 @@ The add_* return instance whether or not it exists, and should persist
 it either way.
 The get_* and *.from_path return only existing instances.
 """
+from typing import Any, Generator, List
+
+from contextlib import contextmanager
+from glob import glob
 import os
 from os.path import join, isfile, isdir, exists
-from glob import glob
 import string
 from threading import Lock
 
 from flask import current_app
 import git
+
+
+# -- oh these guys? haha. they're cool. they're with me.
+
 
 ext = ".md"
 glob_ext = f"*{ext}"
@@ -20,7 +27,7 @@ ext_len = len(ext)
 # -- path library
 
 
-def list_subdir_paths_no_trailing_slash(the_dir):
+def list_subdir_paths_no_trailing_slash(the_dir: str) -> List[str]:
     return [
         join(the_dir, d).rstrip("/")
         for d in os.listdir(the_dir)
@@ -28,7 +35,7 @@ def list_subdir_paths_no_trailing_slash(the_dir):
     ]
 
 
-def list_subdir_names(the_dir):
+def list_subdir_names(the_dir: str) -> List[str]:
     return [d for d in os.listdir(the_dir) if isdir(join(the_dir, d))]
 
 
@@ -43,7 +50,7 @@ class ImSoryButNo(ImSory):
     pass
 
 
-def ass(condition, message, no=False):
+def ass(condition: Any, message: str, no=False) -> None:
     if condition:
         return
 
@@ -64,22 +71,19 @@ except git.InvalidGitRepositoryError:
     repo = git.Repo.init(current_app.instance_path)
 
 
-class commit_txn:
-    def __init__(self, path, commit_message):
-        self.path = path
-        self.commit_message = commit_message
-
-    def __enter__(self):
-        ass(not repo.index.diff(None), "I don't want to mess with that.")
-        lock.acquire()
-
-    def __exit__(self):
-        repo.index.add([self.path])
+@contextmanager
+def commit_txn(path: str, commit_message: str) -> Generator[None, None, None]:
+    ass(not repo.index.diff(None), "I don't want to mess with that.")
+    lock.acquire()
+    try:
+        yield
+    finally:
+        repo.index.add([path])
         ass(
             len(repo.index.diff(None)) == 1,
             "Whoah there. One thing at a time.",
         )
-        repo.index.commit(self.commit_message)
+        repo.index.commit(commit_message)
         lock.release()
 
 
@@ -90,7 +94,7 @@ class card:
 
     chars = string.digits + string.ascii_letters + " "
 
-    def __init__(self, name, column_root):
+    def __init__(self, name: str, column_root: str) -> None:
         assert isdir(column_root)
         assert len(name) <= 61
         self.name = name
@@ -98,7 +102,7 @@ class card:
         self.path = join(column_root, f"{name}{ext}")
         self._validate()
 
-    def _validate(self):
+    def _validate(self) -> None:
         ass(
             all(c in self.chars for c in self.name),
             f"Invalid name {self.name} for card.",
@@ -123,11 +127,11 @@ class card:
         else:
             open(self.path, "w").close()
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return isinstance(other, card) and other.path == self.path
 
     @property
-    def content(self):
+    def content(self) -> str:
         self._validate()
         with lock:
             with open(self.path, "r") as card_md:
@@ -135,14 +139,14 @@ class card:
         return content
 
     @content.setter
-    def content(self, value):
+    def content(self, value: str) -> None:
         ass(isinstance(value, str), "Um, string please?")
-        with commit_txn(f"Update card {self.name}."):
+        with commit_txn(self.path, f"Update card {self.name}."):
             with open(self.path, "w") as card_md:
                 card_md.write(value)
 
-    @classmethod
-    def from_filename(cls, filename):
+    @staticmethod
+    def from_filename(filename: str) -> "card":
         column_root, name_md = os.path.split(filename)
         assert column_root
         assert name_md.endswith(ext)
@@ -154,14 +158,14 @@ class column:
 
     chars = string.digits + string.ascii_letters + " "
 
-    def __init__(self, name, board_root):
+    def __init__(self, name: str, board_root: str) -> None:
         assert isdir(board_root)
         self.name = name
         self.board_root = board_root
         self.path = join(board_root, name)
         self._validate()
 
-    def _validate(self):
+    def _validate(self) -> None:
         ass(
             all(c in self.chars for c in self.name),
             f"{self.name} invalid name for column.",
@@ -187,46 +191,45 @@ class column:
         else:
             open(join(self.path, ".index"), "w").close()
 
-    def __contains__(self, other):
-        if isinstance(other, card):
-            return card in self.cards
+    def __contains__(self, other: Any) -> bool:
+        return isinstance(other, card) and card in self.cards
 
     @property
-    def cards(self):
+    def cards(self) -> List[card]:
         self._validate()
         return [card.from_filename(f) for f in glob(join(self.path, glob_ext))]
 
-    def get_card(self, name):
+    def get_card(self, name: str) -> card:
         return card.from_filename(join(self.path, f"{name}{ext}"))
 
-    def add_card(self, name):
+    def add_card(self, name: str) -> card:
         try:
             return self.get_card(name)
         except ImSoryButNo:
-            with commit_txn(f"Column {self.name} add card {name}."):
+            with commit_txn(self.path, f"Column {self.name} add card {name}."):
                 k = card(name, self.path)
             return k
 
-    @classmethod
-    def from_path(cls, path):
+    @staticmethod
+    def from_path(path: str) -> "column":
         assert not path.endswith("/")
         ass(isdir(path), "from_path but it didn't exist.", no=True)
-        board_root, card_dir = os.path.split(path)
-        return cls(card_dir, board_root)
+        board_root, column_dir = os.path.split(path)
+        return column(column_dir, board_root)
 
 
 class board:
 
     chars = string.ascii_lowercase
 
-    def __init__(self, name: str, root: str):
+    def __init__(self, name: str, root: str) -> None:
         assert exists(root)
         self.name = name
         self.root = root
         self.path = join(root, name)
         self._validate()
 
-    def _validate(self):
+    def _validate(self) -> None:
         ass(self.root, f"Bad board root {self.root}.")
         ass(
             isdir(self.root), f"Board root {self.root} not a directory.",
@@ -251,52 +254,52 @@ class board:
         else:
             open(join(self.path, ".keep"), "w").close()
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return isinstance(other, board) and self.path == other.path
 
     @property
-    def columns(self):
+    def columns(self) -> List[column]:
         return [
             column.from_path(p)
             for p in list_subdir_paths_no_trailing_slash(self.path)
         ]
 
-    def get_column(self, name):
+    def get_column(self, name: str) -> column:
         return column.from_path(join(self.path, name))
 
-    def add_column(self, name):
+    def add_column(self, name: str) -> column:
         try:
             return self.get_column(name)
         except ImSoryButNo:
-            with commit_txn(f"Board {self.name} add column {name}"):
+            with commit_txn(self.path, f"Board {self.name} add column {name}"):
                 c = column(name, self.path)
             return c
 
-    @classmethod
-    def from_path(cls, path):
+    @staticmethod
+    def from_path(path: str) -> "board":
         assert not path.endswith("/")
         ass(isdir(path), "from_path but it didn't exist.", no=True)
         db_root, board_dir = os.path.split(path)
-        return cls(board_dir, db_root)
+        return board(board_dir, db_root)
 
 
 # -- global model api
 
 
-def add_board(name):
+def add_board(name: str) -> board:
     try:
         return get_board(name)
     except ImSoryButNo:
-        with commit_txn(f"Add board {name}."):
+        with commit_txn(current_app.instance_path, f"Add board {name}."):
             b = board(name, current_app.instance_path)
         return b
 
 
-def get_board(name):
+def get_board(name: str) -> board:
     return board.from_path(join(current_app.instance_path, name))
 
 
-def _boards():
+def _boards() -> List[board]:
     return [
         board.from_path(p)
         for p in list_subdir_paths_no_trailing_slash(current_app.instance_path)
